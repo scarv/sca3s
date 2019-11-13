2c30703faae7f201a2f815759b041b72b3d2cc32
Dan Page <dan@phoo.org>
2019-11-13 08:34:53 +0000

    refactor

---
+++ bin/conf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export REPO_HOME="${PWD}"
+
+export REPO_VERSION_MAJOR="0"
+export REPO_VERSION_MINOR="1"
+export REPO_VERSION_PATCH="0"
+
+export REPO_VERSION="${REPO_VERSION_MAJOR}.${REPO_VERSION_MINOR}.${REPO_VERSION_PATCH}"
+
+export TEXMFLOCAL="${TEXMFLOCAL}:${REPO_HOME}/extern/texmf"
